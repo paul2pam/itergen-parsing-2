2f72065db82708b32d23b442bae375ce86ba2de9
Paul <paul2pam@yahoo.com>
2026-03-31 12:47:28 -0500

    Fixing dependencies

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'itergen'))
+
+from itergen.main import IterGen
+
+with open(os.path.join(os.path.dirname(__file__), 'grammar.txt')) as f:
+    grammar = f.read()
 
