c5fb4c4ccd02116c5d7198c28547848322f2c64c
Paul <paul2pam@yahoo.com>
2026-03-31 13:51:07 -0500

    Tools.txt implemented

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ from itergen.main import IterGen
 with open(os.path.join(os.path.dirname(__file__), 'grammar.txt')) as f:
     grammar = f.read()
 
+iter_gen = IterGen(
+    grammar=grammar,
+    model_id="microsoft/Phi-3-mini-128k-instruct",
+    device='cuda',
+    quantize=True,
+)
+
